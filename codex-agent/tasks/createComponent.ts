d71309dd716e38d0c54dbc928db96a8aa9263a83
Joe <joseph.a.whittle@gmail.com>
2025-06-02 06:28:37 -0500

    chore: add codex agent scaffolding

---
+++ codex-agent/tasks/createComponent.ts
@@ -0,0 +1,4 @@
+export async function createComponent({ name, props }: { name: string; props: string[] }) {
+  console.log(`[Codex] Creating component ${name} with props:`, props);
+  // TODO: Generate file dynamically
+}
